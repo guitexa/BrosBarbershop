dd552cfced764f95757d2994b2c69493ae49eb0e
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-06-23 15:26:48 -0300

    Dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
+
+import logoImg from '../../assets/logo.png';
+import bgdark from '../../assets/bgdark.png';
+
+import { ContainerImgBg, DivImg, Text, TextButton } from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut } = useAuth();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button onPress={signOut} title="Sair" />
-    </View>
+    <ContainerImgBg source={bgdark}>
+      <DivImg>
+        <Image source={logoImg} />
+      </DivImg>
+      <Text>SEJA BEM-VINDO</Text>
+      <TouchableOpacity onPress={signOut}>
+        <TextButton>SAIR</TextButton>
+      </TouchableOpacity>
+    </ContainerImgBg>
   );
 };
 
