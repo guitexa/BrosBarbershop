6a6db091099e9cf04960a6a6110d0bad4d290c3a
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-08-18 16:55:40 -0300

    feat: created page to confirm appointment

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import React, { useMemo, useEffect, useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { useAuth } from '../../hooks/auth';
-
-import logoImg from '../../assets/logo.png';
 import bgdark from '../../assets/bgdark.png';
 
-import { ContainerImgBg, DivImg, Text, TextButton } from './styles';
+import {
+  ContainerImgBg,
+  AppointmentConfirmation,
+  AppointmentTime,
+  OkButton,
+  OkButtonText,
+} from './styles';
+import { BackHandler } from 'react-native';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { params } = useRoute();
+  const { date } = params as RouteParams;
+
+  const { navigate } = useNavigation();
+
+  //Função para alterar o comportamento do botão de voltar do Android
+  useEffect(
+    useCallback(() => {
+      const onBackPress = () => {
+        navigate('Dashboard');
+        return true;
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, [navigate])
+  );
+
+  const formattedDate = useMemo(() => {
+    const newDate = format(
+      date,
+      "cccc'-feira, dia' dd 'de' MMMM 'de' yyyy 'às' HH:'00'",
+      {
+        locale: ptBR,
+      }
+    );
+
+    const uppercaseFirstLetter = newDate.charAt(0).toUpperCase();
+    const stringWithoutFirstLetter = newDate.slice(1);
+
+    return `${uppercaseFirstLetter}${stringWithoutFirstLetter}`;
+  }, [date]);
 
   return (
     <ContainerImgBg source={bgdark}>
-      <DivImg>
-        <Image source={logoImg} />
-      </DivImg>
-      <Text>AppointmentCreated</Text>
-      <TouchableOpacity onPress={signOut}>
-        <TextButton>SAIR</TextButton>
-      </TouchableOpacity>
+      <Icon name="check" size={120} color="#1c8f07" />
+      <AppointmentConfirmation>
+        Agendamento realizado com sucesso
+      </AppointmentConfirmation>
+      <AppointmentTime>{formattedDate}</AppointmentTime>
+      <OkButton onPress={() => navigate('Dashboard')}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
     </ContainerImgBg>
   );
 };
--- mobile/src/pages/AppointmentCreated/styles.ts
+++ mobile/src/pages/AppointmentCreated/styles.ts
@@ -2,30 +2,41 @@ import styled from 'styled-components/native';
 
 export const ContainerImgBg = styled.ImageBackground`
   flex: 1;
-  justify-content: center;
   align-items: center;
-  padding: 0 40px 40px;
+  padding: 0 40px;
   resize-mode: cover;
   justify-content: center;
 `;
 
-export const DivImg = styled.View`
-  margin-bottom: 25px;
-`;
-
-export const Text = styled.Text`
-  margin-bottom: 25px;
+export const AppointmentConfirmation = styled.Text`
   font-family: 'Teko-Medium';
-  font-size: 40px;
+  font-size: 35px;
+  line-height: 30px;
+  padding-top: 10px;
+  text-align: center;
+  margin: 20px 0;
   color: #eee;
 `;
 
-export const TextButton = styled.Text`
+export const AppointmentTime = styled.Text`
+  font-family: 'Teko-Light';
+  font-size: 29px;
+  color: #aaa;
+  line-height: 28px;
+  padding-top: 10px;
+  text-align: center;
   margin-bottom: 20px;
+`;
+
+export const OkButton = styled.TouchableOpacity`
+  padding: 10px 50px;
+  background: #eee;
+  border-radius: 8px;
+`;
+
+export const OkButtonText = styled.Text`
   font-family: 'Teko-Medium';
-  font-size: 18px;
-  background-color: #eee;
+  font-size: 28px;
+  height: 34px;
   color: #000;
-  border-radius: 6px;
-  padding: 6px 60px 3px;
 `;
