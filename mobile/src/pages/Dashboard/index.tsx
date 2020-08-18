508c5a07d80f1fd00977dbf1126f0d9b0f89ec3a
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-08-18 16:54:53 -0300

    feat: created dashboard page

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,25 +1,138 @@
-import React from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { Alert, BackHandler } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
-
-import logoImg from '../../assets/logo.png';
+import api from '../../services/api';
 import bgdark from '../../assets/bgdark.png';
 
-import { ContainerImgBg, DivImg, Text, TextButton } from './styles';
+import {
+  ContainerImgBg,
+  Header,
+  ContentHeader,
+  HeaderText,
+  Welcome,
+  UserName,
+  UserAvatar,
+  Avatar,
+  ProvidersList,
+  Title,
+  ProviderContainer,
+  ProviderAvatar,
+  InfoProvider,
+  ProviderName,
+  MetaInfo,
+  Availability,
+  AvailabilityText,
+  Hour,
+  HourText,
+  Footer,
+} from './styles';
+
+export interface Provider {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  //Estado para armazenar os providers recebidos da API
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { user } = useAuth();
+
+  const { navigate } = useNavigation();
+
+  //Chamada à API para trazer os providers
+  useEffect(() => {
+    api
+      .get<Provider[]>('/providers')
+      .then((response) => setProviders(response.data));
+  }, []);
+
+  //Função para controlar o botão de voltar do Android
+  useEffect(
+    useCallback(() => {
+      const onBackPress = () => {
+        Alert.alert(
+          'Ops!',
+          'Deseja sair da aplicação?',
+          [
+            {
+              text: 'Não',
+              onPress: () => null,
+              style: 'cancel',
+            },
+            {
+              text: 'Sim',
+              onPress: () => BackHandler.exitApp(),
+            },
+          ],
+          { cancelable: true }
+        );
+        return true;
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, [])
+  );
+
+  //Função para navegar para o profile ao clicar na foto
+  const handleNavigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, []);
+
+  //Função que recebe o provider_id e navega para página de criação do agendamento
+  const handleCreateAppointment = useCallback((provider_id: string) => {
+    navigate('CreateAppointment', { provider_id });
+  }, []);
 
   return (
     <ContainerImgBg source={bgdark}>
-      <DivImg>
-        <Image source={logoImg} />
-      </DivImg>
-      <Text>SEJA BEM-VINDO</Text>
-      <TouchableOpacity onPress={signOut}>
-        <TextButton>SAIR</TextButton>
-      </TouchableOpacity>
+      <Header>
+        <ContentHeader>
+          <HeaderText>
+            <Welcome>Bem vindo,</Welcome>
+            <UserName>{user.name}</UserName>
+          </HeaderText>
+          <UserAvatar onPress={handleNavigateToProfile}>
+            <Avatar source={{ uri: user.avatar_url }} />
+          </UserAvatar>
+        </ContentHeader>
+      </Header>
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        ListHeaderComponent={<Title>Cabeleireiros</Title>}
+        ListFooterComponent={<Footer />}
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => {
+              handleCreateAppointment(provider.id);
+            }}
+          >
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+            <InfoProvider>
+              <ProviderName>{provider.name}</ProviderName>
+              <MetaInfo>
+                <Availability>
+                  <Icon name="calendar" size={15} color="#aaa" />
+                  <AvailabilityText>Segunda à sexta-feira</AvailabilityText>
+                </Availability>
+                <Hour>
+                  <Icon name="clock" size={15} color="#aaa" />
+                  <HourText>8h às 17h</HourText>
+                </Hour>
+              </MetaInfo>
+            </InfoProvider>
+          </ProviderContainer>
+        )}
+      />
     </ContainerImgBg>
   );
 };
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,31 +1,113 @@
 import styled from 'styled-components/native';
+import { RectButton, FlatList } from 'react-native-gesture-handler';
+
+import { Provider } from './index';
 
 export const ContainerImgBg = styled.ImageBackground`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  padding: 0 40px 40px;
   resize-mode: cover;
-  justify-content: center;
 `;
 
-export const DivImg = styled.View`
-  margin-bottom: 25px;
+export const Header = styled.View`
+  background: #00000090;
+  padding: 20px 20px;
 `;
 
-export const Text = styled.Text`
-  margin-bottom: 25px;
-  font-family: 'Teko-Medium';
-  font-size: 40px;
+export const ContentHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderText = styled.View``;
+
+export const Welcome = styled.Text`
+  font-family: 'Teko-Regular';
+  font-size: 24px;
+  height: 24px;
+  color: #aaa;
+`;
+
+export const UserName = styled.Text`
+  font-family: 'Teko-Regular';
+  font-size: 27px;
   color: #eee;
 `;
 
-export const TextButton = styled.Text`
-  margin-bottom: 20px;
-  font-family: 'Teko-Medium';
-  font-size: 18px;
-  background-color: #eee;
-  color: #000;
-  border-radius: 6px;
-  padding: 6px 60px 3px;
+export const UserAvatar = styled.TouchableOpacity``;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Teko-Regular';
+  font-size: 28px;
+  color: #eee;
+`;
+
+export const ProviderContainer = styled.TouchableOpacity`
+  margin: 10px 0;
+  flex-direction: row;
+  align-items: center;
+  padding: 15px 20px;
+  border-radius: 10px;
+  background: rgba(0, 0, 0, 0.7);
+  border-width: 1px;
+  border-color: #eeeeee50;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  margin-right: 20px;
+`;
+
+export const InfoProvider = styled.View``;
+
+export const ProviderName = styled.Text`
+  font-family: 'Teko-Regular';
+  font-size: 30px;
+  height: 34px;
+  color: #eee;
+`;
+
+export const MetaInfo = styled.View``;
+
+export const Availability = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const AvailabilityText = styled.Text`
+  font-family: 'Teko-Light';
+  font-size: 20px;
+  color: #888;
+  margin-left: 10px;
+  margin-top: 3px;
+`;
+
+export const Hour = styled.View`
+  flex-direction: row;
+  height: 17px;
+  align-items: center;
+`;
+
+export const HourText = styled.Text`
+  font-family: 'Teko-Light';
+  font-size: 20px;
+  color: #888;
+  margin-left: 10px;
+  margin-top: 3px;
+`;
+
+export const Footer = styled.View`
+  padding: 15px;
 `;
