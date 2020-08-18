dfe57db4ffb3d33556f53ab98b73df1e60f6fbb2
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-08-18 16:55:57 -0300

    feat: create page to view and update profile

--- mobile/src/pages/Profile/index.tsx
+++ mobile/src/pages/Profile/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import {
-  Image,
   Keyboard,
   ScrollView,
   KeyboardAvoidingView,
@@ -12,62 +11,170 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import ImagePicker from 'react-native-image-picker';
 
 import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import logoImg from '../../assets/logo.png';
 import bgdark from '../../assets/bgdark.png';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import { useAuth } from '../../hooks/auth';
 
 import {
   ContainerImgBg,
-  DivImg,
-  BackToSignIn,
-  BackToSignInText,
-  FullButton,
+  ContainerScroll,
+  Header,
+  BackButton,
+  UserAvatar,
+  CameraButton,
+  Avatar,
+  SignOutButton,
+  SignOutButtonText,
 } from './styles';
 
 interface DataForm {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
   const [open, setOpen] = useState(false);
 
-  const navigation = useNavigation();
+  const { user, signOut, updateUser } = useAuth();
+
+  const { reset, goBack } = useNavigation();
 
   const formRef = useRef<FormHandles>(null);
+
+  const handleAvatarChange = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione uma imagem',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher na galeria',
+      },
+      (response) => {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          Alert.alert('Erro ao atualizar avatar');
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api
+          .patch('/users/avatar', data)
+          .then((apiResponse) => updateUser(apiResponse.data));
+
+        Alert.alert('Sucesso!', 'Avatar atualizado com sucesso');
+      }
+    );
+  }, [updateUser, user.id]);
 
   const handleSubmit = useCallback(
     async (data: DataForm) => {
       try {
         formRef.current?.setErrors({});
 
-        const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório'),
-          email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'Senha deve ter no mínimo 6 dígitos'),
-        });
+        const schema = Yup.object().shape(
+          {
+            name: Yup.string(),
+            email: Yup.string().email('Digite um e-mail válido'),
+            old_password: Yup.string().when(
+              ['password', 'password_confirmation'],
+              {
+                is: (val) => !!val,
+                then: Yup.string().required('Senha obrigatória'),
+              }
+            ),
+            password: Yup.string().when(
+              ['old_password', 'password_confirmation'],
+              {
+                is: (val) => !!val,
+                then: Yup.string()
+                  .required('Senha obrigatória')
+                  .min(6, 'Senha deve ter no mínimo 6 dígitos'),
+              }
+            ),
+            password_confirmation: Yup.string()
+              .when(['password', 'old_password'], {
+                is: (val) => !!val,
+                then: Yup.string().required('Campo obrigatório'),
+              })
+              .oneOf([Yup.ref('password')], 'Senha redigitada não confere'),
+          },
+          [
+            ['password', 'password_confirmation'],
+            ['old_password', 'password_confirmation'],
+            ['password', 'old_password'],
+          ]
+        );
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
 
-        Alert.alert(
-          'Usuário cadastrado com sucesso',
-          'Agora você pode fazer login na aplicação'
-        );
+        const formData = {
+          ...(name ? { name } : {}),
+          ...(email ? { email } : {}),
+          ...(name && email ? { name, email } : {}),
+          ...(name && old_password
+            ? { name, old_password, password, password_confirmation }
+            : {}),
+          ...(email && old_password
+            ? { email, old_password, password, password_confirmation }
+            : {}),
+          ...(name && email && old_password
+            ? { name, email, old_password, password, password_confirmation }
+            : {}),
+          ...(old_password
+            ? { old_password, password, password_confirmation }
+            : {}),
+        };
 
-        navigation.goBack();
+        if (Object(formData).length === 0) {
+          return Alert.alert(
+            'Erro',
+            'Para atualizar seu cadastro você deve preencher algum dos campos'
+          );
+        }
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
+
+        Alert.alert('Sucesso!!!', 'Perfil atualizado com sucesso!');
+
+        reset({
+          index: 0,
+          routes: [
+            {
+              name: 'Dashboard',
+            },
+          ],
+        });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -77,10 +184,13 @@ const Profile: React.FC = () => {
           return;
         }
 
-        Alert.alert('Erro no cadastro', 'Verifique os dados e tente novamente');
+        Alert.alert(
+          'Erro na atualização',
+          'Verifique as informações e tente novamente'
+        );
       }
     },
-    [navigation]
+    [reset, updateUser]
   );
 
   useEffect(() => {
@@ -108,12 +218,20 @@ const Profile: React.FC = () => {
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
         enabled
       >
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <ContainerImgBg source={bgdark}>
+        <ContainerImgBg source={bgdark}>
+          <ContainerScroll>
             {!open && (
-              <DivImg>
-                <Image source={logoImg} />
-              </DivImg>
+              <Header>
+                <BackButton onPress={() => goBack()}>
+                  <Icon name="chevron-left" size={30} color="#aaa" />
+                </BackButton>
+                <UserAvatar>
+                  <CameraButton onPress={handleAvatarChange}>
+                    <Icon name="camera" size={30} color="#000" />
+                  </CameraButton>
+                  <Avatar source={{ uri: user.avatar_url }} />
+                </UserAvatar>
+              </Header>
             )}
             <Form
               style={{ width: '100%' }}
@@ -124,6 +242,8 @@ const Profile: React.FC = () => {
                 name="name"
                 icon="user"
                 placeholder="NOME"
+                returnKeyType="next"
+                defaultValue={user.name}
                 onSubmitEditing={Keyboard.dismiss}
               />
               <Input
@@ -132,13 +252,32 @@ const Profile: React.FC = () => {
                 placeholder="E-MAIL"
                 autoCapitalize="none"
                 autoCorrect={false}
+                returnKeyType="next"
+                defaultValue={user.email}
                 keyboardType="email-address"
+                onSubmitEditing={Keyboard.dismiss}
+              />
+              <Input
+                styled={{ marginTop: 30 }}
+                name="old_password"
+                icon="lock"
+                placeholder="SENHA ATUAL"
+                secureTextEntry
+                returnKeyType="next"
                 onSubmitEditing={Keyboard.dismiss}
               />
               <Input
                 name="password"
                 icon="lock"
-                placeholder="SENHA"
+                placeholder="NOVA SENHA"
+                secureTextEntry
+                returnKeyType="next"
+                onSubmitEditing={Keyboard.dismiss}
+              />
+              <Input
+                name="password_confirmation"
+                icon="lock"
+                placeholder="CONFIRMAR NOVA SENHA"
                 secureTextEntry
                 returnKeyType="send"
                 onSubmitEditing={() => {
@@ -151,22 +290,16 @@ const Profile: React.FC = () => {
                   formRef.current?.submitForm();
                 }}
               >
-                Criar conta grátis
+                Atualizar perfil
               </Button>
             </Form>
-          </ContainerImgBg>
-        </ScrollView>
+            <SignOutButton onPress={signOut}>
+              <SignOutButtonText>SAIR DA APLICAÇÃO</SignOutButtonText>
+              <Icon name="log-out" size={20} color="#eeeeee90" />
+            </SignOutButton>
+          </ContainerScroll>
+        </ContainerImgBg>
       </KeyboardAvoidingView>
-      <BackToSignIn>
-        <FullButton
-          onPress={() => {
-            navigation.goBack();
-          }}
-        >
-          <Icon name="arrow-left" size={20} color="#eee" />
-          <BackToSignInText>Voltar para página de login</BackToSignInText>
-        </FullButton>
-      </BackToSignIn>
     </>
   );
 };
--- mobile/src/pages/Profile/styles.ts
+++ mobile/src/pages/Profile/styles.ts
@@ -1,51 +1,87 @@
 import styled from 'styled-components/native';
 
-import { getBottomSpace } from 'react-native-iphone-x-helper';
-
-export const Text = styled.Text`
-  color: #eee;
-  font-family: 'Teko-Medium';
-  font-size: 20px;
-  margin-top: 40px;
-`;
-
-export const DivImg = styled.View`
-  margin-bottom: 25px;
-`;
-
 export const ContainerImgBg = styled.ImageBackground`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 0 40px 40px;
   resize-mode: cover;
-  justify-content: center;
 `;
 
-export const BackToSignIn = styled.View`
-  position: absolute;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  padding: 10px 0 ${10 + getBottomSpace()}px;
+export const ContainerScroll = styled.ScrollView`
   width: 100%;
-  height: 50px;
-  border-top-width: 1px;
-  border-color: #eee;
-  background-color: #060505;
+  padding: 0 40px;
 `;
 
-export const FullButton = styled.TouchableOpacity`
-  flex: 1;
+export const Header = styled.View`
+  width: 100%;
+  margin-top: 40px;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  flex-direction: row;
+  position: relative;
 `;
 
-export const BackToSignInText = styled.Text`
-  margin-left: 10px;
-  font-family: 'Teko-Light';
-  font-size: 20px;
-  text-transform: uppercase;
+export const BackButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: -5px;
+  padding: 0 15px 15px 0;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Teko-Regular';
+  font-size: 30px;
   color: #eee;
+  height: 33px;
+  margin-left: 20px;
+`;
+
+export const UserAvatar = styled.View`
+  margin-bottom: 25px;
+`;
+
+export const Avatar = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+`;
+
+export const CameraButton = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  padding-bottom: 2px;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background: #eee;
+  border-width: 2px;
+  border-color: #000;
+  z-index: 9999;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+  flex-direction: row;
+  width: 100%;
+  border-top-width: 1px;
+  border-color: #aaaaaa50;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  padding-top: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignOutButtonText = styled.Text`
+  margin-right: 10px;
+  font-family: 'Teko-Light';
+  font-size: 22px;
+  margin-top: 3px;
+  text-transform: uppercase;
+  color: #eeeeee90;
 `;
